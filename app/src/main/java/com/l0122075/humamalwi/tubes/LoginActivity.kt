1c88f50aae2e12d6957b0524117aea9e2b89bbbd
humamalwi <alwia901@gmail.com>
2024-06-08 16:33:29 +0700

    menu admin

--- app/src/main/java/com/l0122075/humamalwi/tubes/LoginActivity.kt
+++ app/src/main/java/com/l0122075/humamalwi/tubes/LoginActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.l0122075.humamalwi.tubes.databinding.ActivityLoginBinding
 
